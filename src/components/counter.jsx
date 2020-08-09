9e8c41c4f21b756aef065061766828851c8ed058
gkaragoz <gkaragoz96@gmail.com>
2020-08-09 12:08:27 +0300

    Add handle increment function to counters instead of counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,22 +1,15 @@
 import React, { Component } from "react";
 
 export class Counter extends Component {
-  state = {
-    value: this.props.counter.value,
-  };
-
-  handleIncrement = () => {
-    this.setState({ value: this.state.value + 1 });
-  };
-
   getBadgeClasses = () => {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   };
 
   formatValue = () => {
-    return this.state.value === 0 ? "Zero" : this.state.value;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   };
 
   render() {
@@ -24,7 +17,7 @@ export class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatValue()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -5,11 +5,29 @@ import Counter from "./counter";
 export class Counters extends Component {
   state = {
     counters: [
-      { id: 1, value: 0 },
+      { id: 1, value: 4 },
       { id: 2, value: 0 },
       { id: 3, value: 0 },
       { id: 4, value: 0 },
     ],
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+
+    this.setState({ counters });
   };
 
   handleDelete = (counterId) => {
@@ -20,9 +38,16 @@ export class Counters extends Component {
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
+            onIncrement={this.handleIncrement}
             onDelete={this.handleDelete}
             counter={counter}
           />
